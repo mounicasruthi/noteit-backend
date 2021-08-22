013c9be7645990be6fa231f5a5fa77103f603f3c
Mounica <mounica@pop-os.localdomain>
2021-08-22 18:14:04 +0530

    almost done

---
+++ controllers/auth.js
@@ -0,0 +1,108 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const client = require("../configs/db");
+
+exports.signUp = (req, res) => {
+  const { name, email, password } = req.body;
+
+   client
+    .query(`SELECT * FROM users WHERE email = '${email}';`)
+    .then((data) => {
+      isValid = data.rows;
+
+        if (isValid.length !== 0) {
+        res.status(400).json({
+          error: "User already exists.",
+        });
+      } else {
+        bcrypt.hash(password, 10, (err, hash) => {
+          if (err) {
+            res.status(500).json({
+              error: "Internal server error.",
+            });
+          }
+
+          const user = {
+            name,
+            email,
+            password: hash,
+          };
+
+          client
+            .query(
+              `INSERT INTO users (name, email, password) VALUES ('${user.name}', '${user.email}' , '${user.password}');`
+            )
+            .then((data) => {
+                const token = jwt.sign(
+                {
+                  email: email,
+                },
+                process.env.SECRET_KEY
+              );
+
+              res.status(200).json({
+                message: "User added successfully to database",
+                token: token,
+              });
+            })
+            .catch((err) => {
+                res.status(500).json({
+                error: "Database error occurred!",
+              });
+            });
+        });
+      }
+    })
+    .catch((err) => {
+        res.status(500).json({
+        error: "Database error occurred!",
+      });
+    });
+};
+
+exports.signIn = (req, res) => {
+    const { email, password } = req.body;
+
+    client
+    .query(`SELECT * FROM users WHERE email = '${email}';`)
+    .then((data) => {
+      userData = data.rows;
+     
+      if (userData.length === 0) {
+        
+        res.status(400).json({
+          error: "User does not exist, signup instead!",
+        });
+      } else {
+       
+        bcrypt.compare(password, userData[0].password, (err, result) => {
+          if (err) {
+           res.status(500).json({
+              error: "Server error!",
+            });
+          } else if (result === true) {
+            const token = jwt.sign(
+              {
+                email: email,
+              },
+              process.env.SECRET_KEY
+            );
+            res.status(200).json({
+              message: "User signed in successfully",
+              token: token,
+            });
+          } else {
+             res.status(400).json({
+              error: "Enter correct password!",
+            });
+          }
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: "Database error occurred!",
+      });
+    });
+};
+
---
+++ routes/notes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const { addNote, getAllNotes, updateNote } = require("../controllers/notes");
+const { verifyToken } = require("../middlewares/authMiddleware");
+const { handleNoteIdParam } = require("../middlewares/noteMiddleware");
+const router = express.Router();
+
+router.param("noteId", handleNoteIdParam);
+
+router.post("/add", verifyToken, addNote);
+router.put("/update/:noteId", verifyToken, updateNote);
+router.delete("/delete/:noteId", verifyToken, deleteNote);
+router.get("/getallnotes", verifyToken, getAllNotes);
+
+module.exports = router;
